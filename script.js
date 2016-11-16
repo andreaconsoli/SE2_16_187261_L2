e7bf25bb1afbf357d6ce3ecad949bfa66f2a139e
andreaconsoli <andrea.consoli@studenti.unitn.it>
2016-11-16 15:19:36 +0100

    Aggiunto il file js

---
+++ script.js
@@ -0,0 +1,8 @@
+function visualizza(){
+	document.getElementById("nomeprodotto").style.visibility =  "visible";
+	document.getElementById("quantita").style.visibility =  "visible";
+	document.getElementById("invia").style.visibility =  "visible";
+}
+
+
+
