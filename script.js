5d4cadedfe7e1c75cc6a72dec6c4ef0797d1eb51
andreaconsoli <andrea.consoli@studenti.unitn.it>
2016-11-16 21:53:49 +0100

    Aggiornate pagine index e js

--- script.js
+++ script.js
@@ -4,5 +4,16 @@ function visualizza(){
 	document.getElementById("invia").style.visibility =  "visible";
 }
 
-
+function inserisciprodotto(){
+	var nomeprodotto, quantita, tabella, riga;
+	nomeprodotto = document.getElementById("nomeprodotto").value;
+	quantita = parseInt(document.getElementById("quantita").value);
+	tabella = document.getElementById("tabellamagazzino");
+	riga = tabella.insertRow(0)
+	var cell1 = riga.insertCell(0);
+	var cell2 = riga.insertCell(1);
+	cell1.innerHTML = nomeprodotto;
+	cell2.innerHTML = quantita;	
+	
+}
 
